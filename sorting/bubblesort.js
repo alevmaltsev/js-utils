05f335c51e8fc9baca00a17c4bcae54cbb70c724
Alexander Maltsev <alev.maltsev@gmail.com>
2022-02-18 12:11:07 +0400

    update bubble sort

--- sorting/bubblesort.js
+++ sorting/bubblesort.js
@@ -1,16 +1,15 @@
 export const bubblesort = array => {
-    const shallowCopiedArray = [...array];
-    let length = shallowCopiedArray.length - 1;
+    let length = array.length - 1;
     let swapped;
     do {
         swapped = false;
         for (let i = 0; i < length; i += 1 ) {
-            if(shallowCopiedArray[i] > shallowCopiedArray[i+1]) {
-                [shallowCopiedArray[i], shallowCopiedArray[i+1]] = [shallowCopiedArray[i+1], shallowCopiedArray[i]];
+            if(array[i] > array[i+1]) {
+                [array[i], array[i+1]] = [array[i+1], array[i]];
                 swapped = true;
             }
         }
         length -= 1;
     } while (swapped);
-    return shallowCopiedArray;
+    return array;
 }
